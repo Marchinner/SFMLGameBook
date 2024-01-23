cab3ab792138c862f558bf74461c29a516a4d03f
Felipe Bezerra Freitas <felipefreitasdev@gmail.com>
2024-01-23 20:26:07 -0300

    Resouces

---
+++ 02_Resources/Source/Game.cpp
@@ -0,0 +1,124 @@
+#include "../Include/Game.h"
+#include "../Include/StringHelpers.h"
+
+
+const sf::Time Game::TimePerFrame{ sf::seconds(1.f / 60.f) };
+const float Game::PlayerSpeed{ 50.f };
+
+Game::Game()
+	: mWindow{ sf::VideoMode(800,600), "SFML Game" }
+	, mTexture{}
+	, mFont{}
+	, mStatisticsText{}
+	, mStatisticsUpdateTime{}
+	, mStatisticsNumFrames{ 0 }
+	, mPlayer{}
+	, mIsMovingUp{ false }
+	, mIsMovingDown{ false }
+	, mIsMovingLeft{ false }
+	, mIsMovingRight{ false }
+{
+	// load Statistics fonts
+	if (!mFont.loadFromFile("./02_Resources/Media/Fonts/Sansation.ttf"))
+	{
+		// Handle loading error
+	}
+	mStatisticsText.setFont(mFont);
+	mStatisticsText.setPosition(5.f, 5.f);
+	mStatisticsText.setCharacterSize(12);
+
+	// load textures
+	mPlayer.setPosition(100.f, 100.f);
+}
+
+// Game main loop
+void Game::run()
+{
+	sf::Clock clock;
+	sf::Time timeSinceLastUpdate = sf::Time::Zero;
+	while (mWindow.isOpen())
+	{
+		sf::Time deltaTime = clock.restart();
+		timeSinceLastUpdate += deltaTime;
+		while (timeSinceLastUpdate > TimePerFrame)
+		{
+			timeSinceLastUpdate -= TimePerFrame;
+			processEvents();
+			update(TimePerFrame);
+		}
+		updateStatistics(deltaTime);
+		render();
+	}
+}
+
+// Events loop
+void Game::processEvents()
+{
+	sf::Event event;
+	while (mWindow.pollEvent(event))
+	{
+		switch (event.type)
+		{
+		case sf::Event::KeyPressed:
+			handlePlayerInput(event.key.code, true);
+			break;
+		case sf::Event::KeyReleased:
+			handlePlayerInput(event.key.code, false);
+			break;
+		case sf::Event::Closed:
+			mWindow.close();
+			break;
+		}
+	}
+}
+
+void Game::update(sf::Time deltaTime)
+{
+	sf::Vector2f movement{ 0.f, 0.f };
+	if (mIsMovingUp)
+		movement.y -= PlayerSpeed;
+	if (mIsMovingDown)
+		movement.y += PlayerSpeed;
+	if (mIsMovingLeft)
+		movement.x -= PlayerSpeed;
+	if (mIsMovingRight)
+		movement.x += PlayerSpeed;
+
+	mPlayer.move(movement * deltaTime.asSeconds());
+}
+
+void Game::render()
+{
+	mWindow.clear();
+	mWindow.draw(mPlayer);
+	mWindow.draw(mStatisticsText);
+	mWindow.display();
+}
+
+void Game::updateStatistics(sf::Time deltaTime)
+{
+	mStatisticsUpdateTime += deltaTime;
+	mStatisticsNumFrames += 1;
+
+	if (mStatisticsUpdateTime >= sf::seconds(1.0f))
+	{
+		mStatisticsText.setString(
+			"FPS = " + toString(mStatisticsNumFrames) + "\n" +
+			"UPS = " + toString(mStatisticsUpdateTime.asMicroseconds() / mStatisticsNumFrames) + "us");
+
+		mStatisticsUpdateTime -= sf::seconds(1.0f);
+		mStatisticsNumFrames = 0;
+	}
+}
+
+void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed)
+{
+	if (key == sf::Keyboard::W)
+		mIsMovingUp = isPressed;
+	else if (key == sf::Keyboard::S)
+		mIsMovingDown = isPressed;
+	else if (key == sf::Keyboard::A)
+		mIsMovingLeft = isPressed;
+	else if (key == sf::Keyboard::D)
+		mIsMovingRight = isPressed;
+}
---
+++ 02_Resources/Source/Main.cpp
@@ -0,0 +1,10 @@
+#include "../Include/Game.h"
+
+int main(int argc, char* argv[])
+{
+	Game game;
+
+	game.run();
+
+	return 0;
+}
